aa8ed32a3de5f9407b568afb9ca184e1e3eab7d4
TranBaSon <sontbth1808021@fpt.edu.vn>
2018-12-10 10:36:52 +0700

    edit2

--- page-song/my-song.js
+++ page-song/my-song.js
@@ -19,20 +19,20 @@ function loadSong() {
             var content = "";
             for (var i = 0; i < arraySongs.length; i++) {
                 var song = arraySongs [i];
-                content += '<div class="song-item">';
-                content += '<div class="song-index">' + (i + 1) + '</div>';
-                content += '<div class="song-thumbnail">';
+                content += '<div class="song-item col-12 col-s-12">';
+                content += '<div class="song-index col-1 col-s-1">' + (i + 1) + '</div>';
+                content += '<div class="song-thumbnail col-1 col-s-1">';
                 content += '<img src="' + song.thumbnail + '" alt="">';
                 content += '</div>';
-                content += '<div class="song-infor">';
-                content += '<div class="song-name">' + song.name + '</div>';
+                content += '<div class="song-infor col-s-4 col-4">';
+                content += '<div class="song-name col-12 col-s-12">' + song.name + '</div>';
                 content += '<div class="song-singer">' + song.singer + '</div>';
                 content += '</div>';
                 // cach 1
                 //content += '<div class="song-control " onclick="playSong(\'' + song.link + '\')">Play</div>';
                 //cach 2
-                content += `<div class="song-control" onclick="playSong('${song.link}')">Play</div>`;
-                content += '<div class="song-control"><a href="#">Detail></a></div>';
+                content += `<div class="song-control col-1 col-s-1" onclick="playSong('${song.link}')">Play</div>`;
+                content += '<div class="song-control col-2 col-s-2"><a href="#">Detail></a></div>';
                 content += '</div>';
             }
             document.getElementById('list-song').innerHTML += content;
